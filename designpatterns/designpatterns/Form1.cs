a364cca6becacd47e8b7f901bda30ccd9a101a96
Alex <Alexiakab@hotmail.com>
2018-03-29 11:01:16 +0200

    Classes added

--- designpatterns/designpatterns/Form1.cs
+++ designpatterns/designpatterns/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace designpatterns
@@ -19,7 +12,6 @@ namespace designpatterns
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //test
 
         }
     }
